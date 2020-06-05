1bb3b654283cb68ae9a3455d6095eb01e22056ff
Temmarie <temmar_whyte16@yahoo.com>
2020-06-05 09:32:16 +0100

    finished blog comments feature

--- blogger/config/routes.rb
+++ blogger/config/routes.rb
@@ -1,14 +1,14 @@
-Rails.application.routes.draw do
-  root to: 'articles#index'
-  resources :articles do
-    resources :comments
-  end
-end
-
-# Blogger::Application.routes.draw do
+# Rails.application.routes.draw do
 #   root to: 'articles#index'
-
 #   resources :articles do
 #     resources :comments
 #   end
 # end
+
+Blogger::Application.routes.draw do
+  root to: 'articles#index'
+
+  resources :articles do
+    resources :comments
+  end
+end
